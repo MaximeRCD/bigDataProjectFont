b915d4b861470a65130410dab3db481f67356aab
victorlinard <contact@victor-linard.fr>
2023-04-18 21:13:17 +0200

    finish quiz list and quiz attempt

--- play/models.py
+++ play/models.py
@@ -1,10 +1,12 @@
 from django.db import models
-
+from authenticate.models import User
 # Create your models here
 
 
 class Quiz(models.Model):
     quiz_id = models.IntegerField()
+    user_id = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    quiz_date = models.DateTimeField(auto_now_add=True)
 
 
 class Theme(models.Model):
@@ -28,6 +30,7 @@ class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     text = models.CharField(max_length=500)
     is_correct = models.BooleanField(default=False)
+    user_answer = models.BooleanField(default=False)
 
     def __str__(self):
         return self.text
--- quiz/views.py
+++ quiz/views.py
@@ -1,15 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.db.models import Prefetch
 from django.contrib.auth.decorators import login_required
+from play.models import Quiz, Question, Theme
 
 # Create your views here.
 
 
 @login_required
 def quiz_list(request):
-    return render(request, 'quiz_list.html', context={})
+    """quizzes = Quiz.objects.filter(user_id=request.user).prefetch_related(
+        Prefetch(
+            'question_set',
+            queryset=Question.objects.select_related('theme').distinct('theme'),
+            to_attr='unique_themes'
+        )
+    )"""
+    user = request.user
+    quizzes = Quiz.objects.filter(user_id=request.user)
+
+    # Récupérer les thèmes uniques pour chaque quiz
+    for quiz in quizzes:
+        questions = Question.objects.filter(quiz_id=quiz)
+        themes = Theme.objects.filter(question__in=questions)
+        theme_set = set(theme.name for theme in themes)
+        quiz.unique_themes = ' - '.join(theme_set)
+
+    results = {'percentage': 66.0, 'userAnswers': [1, 3, 2, 4], 'goodAnswers': [1, 1, 2, 3]}
+    results['numberOfGoodAnswer'] = sum(ua == ga for ua, ga in zip(results['userAnswers'], results['goodAnswers']))
+    results['numberOfBadAnswer'] = sum(ua != ga for ua, ga in zip(results['userAnswers'], results['goodAnswers']))
+    return render(request, 'quiz_list.html', context={'quizzes': quizzes, 'results': results})
 
 
 @login_required
 def quiz_attempt(request, quiz_id):
-    # vérifier si le quiz id appartient bien au user
-    return render(request, 'quiz_attempt.html', context={})
+    quiz = Quiz.objects.get(quiz_id=quiz_id)
+    if quiz.user_id == request.user:
+        quiz = Quiz.objects.select_related('user_id').prefetch_related('question_set', 'question_set__answer_set').get(quiz_id=quiz_id)
+        questions = Question.objects.filter(quiz_id=quiz_id)
+        themes = Theme.objects.filter(question__in=questions).distinct()
+        theme_string = ' - '.join([theme.name for theme in themes])
+        return render(request, 'quiz_attempt.html', context={'quiz': quiz, 'themes': theme_string, 'results': {'percentage': 66.0, 'points': 5}})
+    else:
+        return redirect('quiz_list')
