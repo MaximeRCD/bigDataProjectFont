f45502356baba545d426779d0b26c9fd20abf225
victorlinard <contact@victor-linard.fr>
2023-05-04 18:03:04 +0200

    change models

--- play/admin.py
+++ play/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Question, Theme, Answer, Quiz
+from .models import Question, Theme, Quiz
 
 # Register your models here.
 
 admin.site.register(Quiz)
 admin.site.register(Question)
 admin.site.register(Theme)
-admin.site.register(Answer)
--- play/models.py
+++ play/models.py
@@ -4,33 +4,44 @@ from authenticate.models import User
 
 
 class Quiz(models.Model):
-    quiz_id = models.IntegerField()
-    user_id = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    quiz_date = models.DateTimeField(auto_now_add=True)
+    user = models.IntegerField(blank=True, null=True)
+    questionid = models.IntegerField(db_column='questionId', blank=True, null=True)  # Field name made lowercase.
+    iduserresponse = models.IntegerField(db_column='idUserResponse', blank=True, null=True)  # Field name made lowercase.
+    createdat = models.DateTimeField(db_column='createdAt', blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+
+
+class Response(models.Model):
+    response = models.CharField(max_length=100, blank=True, null=True)
+    istrue = models.IntegerField(db_column='isTrue', blank=True, null=True)  # Field name made lowercase.
+    idquestion = models.IntegerField(db_column='idQuestion', blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
 
 
 class Theme(models.Model):
-    name = models.CharField(max_length=50)
-    is_selected = models.BooleanField(default=False)
+    theme = models.CharField(max_length=100, blank=True, null=True)
 
-    def __str__(self):
-        return self.name
+    class Meta:
+        managed = False
+
+
+class Type(models.Model):
+    type = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'type'
 
 
 class Question(models.Model):
-    theme = models.ForeignKey(Theme, on_delete=models.CASCADE)
-    text = models.CharField(max_length=500)
-    quiz_id = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    name = models.CharField(max_length=900, blank=True, null=True)
+    themeid = models.IntegerField(db_column='themeId', blank=True, null=True)  # Field name made lowercase.
+    type = models.IntegerField(blank=True, null=True)
+    createdat = models.DateTimeField(db_column='createdAt', blank=True, null=True)  # Field name made lowercase.
 
-    def __str__(self):
-        return self.text
-
-
-class Answer(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    text = models.CharField(max_length=500)
-    is_correct = models.BooleanField(default=False)
-    user_answer = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.text
+    class Meta:
+        managed = False
