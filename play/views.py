b407048a5d201d971feaf81ff518f847e5c53278
victorlinard <contact@victor-linard.fr>
2023-05-04 22:51:49 +0200

    link new model --> questions, response

--- play/views.py
+++ play/views.py
@@ -17,7 +17,7 @@ def play(request, step):
     if request.method == "POST" and step == "themes":
         checked_boxes = [request.POST[f'theme{i}'] for i in range(1, 5) if request.POST.get(f'theme{i}')]
         response = requests.get('http://127.0.0.1:8000/get_questions/'+','.join(checked_boxes)+'/')
-        request.session['quiz'] = {"state": "not_started", "id": response.json()}
+        request.session['quiz'] = {"state": "not_started", "questions": response.json()['questions']}
         return redirect('play', step="wait-for-quiz")
 
     elif request.method == "POST" and step == "wait-for-quiz":
@@ -26,12 +26,13 @@ def play(request, step):
         return redirect('play', step="quiz")
 
     elif request.method == 'GET' and step == 'quiz':
-        quiz_id = request.session['quiz']['id']
-        quiz = Quiz.objects.filter(quiz_id=quiz_id).prefetch_related('question_set__answer_set')
-        questions = Question.objects.filter(quiz_id=quiz_id)
+        print(request.session['quiz'])
+        questions_ids = request.session['quiz']['questions']
+        questions = Question.objects.filter(id__in=questions_ids).prefetch_related('response_set')
+
         themes = Theme.objects.filter(question__in=questions).distinct()
-        theme_string = ' - '.join([theme.name for theme in themes])
-        return render(request, 'play.html', context={'step': step, 'quiz': quiz, 'themes': theme_string})
+        theme_string = ' - '.join([theme.theme for theme in themes])
+        return render(request, 'play.html', context={'step': step, 'questions': questions, 'themes': theme_string})
 
     elif request.method == 'GET' and step == 'results':
         request.session['quiz']['state'] = "terminated"
@@ -47,7 +48,7 @@ def play(request, step):
 
 
 def get_questions(request, themes):
-    response = '1'
+    response = '{"questions": [1,2,3,4]}'
     return HttpResponse(response)
 
 
