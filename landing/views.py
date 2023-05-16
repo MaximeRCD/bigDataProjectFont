0d232f9a7eea75149f9635f274c082ae7659fce4
victorlinard <contact@victor-linard.fr>
2023-05-16 19:24:25 +0200

    add figures

--- landing/views.py
+++ landing/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from authenticate.models import User
+from play.models import Quiz, Question
 
 # Create your views here.
 
 
 def landing(request):
-    return render(request, 'landing.html', context={})
+    users_number = User.objects.all().count()
+    quiz_number = Quiz.objects.values('quiz_hash').distinct().count()
+    questions_number = Question.objects.all().count()
+    return render(request, 'landing.html', context={'users_number': users_number, 'quiz_number': quiz_number, 'questions_number': questions_number})
